55ab33940bce76d703b7960b7d24bfdc1135d2f8
AVesich <75646434+AVesich@users.noreply.github.com>
2024-10-08 14:50:27 -0400

    Initial Commit

---
+++ Mini/MiniApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiniApp.swift
+//  Mini
+//
+//  Created by Austin Vesich on 10/8/24.
+//
+
+import SwiftUI
+
+@main
+struct MiniApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
