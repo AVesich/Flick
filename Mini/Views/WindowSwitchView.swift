dcd826c83dc05d07749040a42f6d768c8c3f3387
AVesich <75646434+AVesich@users.noreply.github.com>
2024-11-25 03:45:03 -0600

    Added automatic scrolling to elements off of the screen

--- Mini/Views/WindowSwitchView.swift
+++ Mini/Views/WindowSwitchView.swift
@@ -14,29 +14,37 @@ struct WindowSwitchView: View {
 
     var body: some View {
         VStack(spacing: 16.0) {
-            ScrollView(.vertical, showsIndicators: false) {
-                ForEach(Array(windowData.enumerated()), id:\.0) { (index, appAndDesc) in
-                    HStack(spacing: 16.0) {
-                        Image(nsImage: appAndDesc.0)
-                            .resizable()
-                            .frame(width: 30.0, height: 30.0)
-                            .scaledToFit()
-                            .scaleEffect((selectedIndex==index) ? 1.2 : 1.0)
-                        Text(appAndDesc.1)
-                            .lineLimit(2)
-                            .scaleEffect((selectedIndex==index) ? 1.05 : 1.0)
-                        Spacer()
-                    }
-                    .background {
-                        if selectedIndex==index {
-                            RoundedRectangle(cornerRadius: 10.0)
-                                .fill(.secondary.opacity(0.2))
-                                .frame(width: 236.0, height: 42.0)
-//                                .scaleEffect(x: 1.07, y: 1.4)
+            ScrollViewReader { scrollView in
+                ScrollView(.vertical, showsIndicators: false) {
+                    ForEach(Array(windowData.enumerated()), id:\.0) { (index, appAndDesc) in
+                        HStack(spacing: 16.0) {
+                            Image(nsImage: appAndDesc.0)
+                                .resizable()
+                                .frame(width: 30.0, height: 30.0)
+                                .scaledToFit()
+                                .scaleEffect((selectedIndex==index) ? 1.2 : 1.0)
+                            Text(appAndDesc.1)
+                                .lineLimit(2)
+                                .scaleEffect((selectedIndex==index) ? 1.05 : 1.0)
+                            Spacer()
                         }
+                        .background {
+                            if selectedIndex==index {
+                                RoundedRectangle(cornerRadius: 10.0)
+                                    .fill(.secondary.opacity(0.2))
+                                    .frame(width: 236.0, height: 42.0)
+                            }
+                        }
+                        .padding(.bottom, index==windowData.count-1 ? 16.0 : 0.0)
+                        .id(index)
+                    }
+                    .padding(16.0)
+                }
+                .onChange(of: selectedIndex) {
+                    withAnimation(.easeInOut(duration: 0.2)) {
+                        scrollView.scrollTo(selectedIndex, anchor: .bottom)
                     }
                 }
-                .padding(16.0)
             }
         }
         .frame(maxWidth: 256.0, maxHeight: 256.0)
