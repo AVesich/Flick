e2efc19d183c716eb63d7ff71712e9f12db70427
AVesich <75646434+AVesich@users.noreply.github.com>
2025-05-31 21:43:36 -0500

    Revised window finding to use ScreenCaptureKit only

--- Flick/Views/SelectableCell.swift
+++ Flick/Views/SelectableCell.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct HoverCell<Content: View>: View {
+struct SelectableCell<Content: View>: View {
     
     public var hovering: Bool!
     public var content: (_ hovering: Bool) -> Content
@@ -18,7 +18,7 @@ struct HoverCell<Content: View>: View {
 }
 
 #Preview {
-    HoverCell(hovering: true) { selected in
+    SelectableCell(hovering: true) { selected in
         Text("Hello World!")
     }
 }
