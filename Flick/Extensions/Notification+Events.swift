27dfa41bebb8a58ec089374335617adee34fcf67
AVesich <75646434+AVesich@users.noreply.github.com>
2025-06-20 14:10:08 -0600

    Saving progress on focus state refactoring

---
+++ Flick/Extensions/Notification+Events.swift
@@ -0,0 +1,13 @@
+//
+//  Notification+scrollEvents.swift
+//  Flick
+//
+//  Created by Austin Vesich on 6/19/25.
+//
+
+import Foundation
+
+extension Notification.Name {
+    static let didScrollUpNotification = Notification.Name("didScrollUpNotification")
+    static let didScrollDownNotification = Notification.Name("didScrollDownNotification")
+}
---
+++ Flick/Views/WindowStack.swift
@@ -0,0 +1,18 @@
+//
+//  WindowListView.swift
+//  Flick
+//
+//  Created by Austin Vesich on 6/20/25.
+//
+
+import SwiftUI
+
+struct WindowStack: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WindowStack()
+}
